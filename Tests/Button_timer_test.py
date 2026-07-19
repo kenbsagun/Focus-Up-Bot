2d56064b088dfe991e0f9a35e82810ba46ba3316
Kenneth Sagun <kenbsagun@gmail.com>
2026-07-19 13:58:30 -0700

    Testing out the import time feature, adding a feature where after 10 seconds, the face on the OLED will open its eyes. Building towards a "Focus mode."

---
+++ Tests/Button_timer_test.py
@@ -0,0 +1,88 @@
+import board
+import displayio
+import i2cdisplaybus
+import digitalio
+import adafruit_displayio_ssd1306
+import time
+
+displayio.release_displays()
+
+i2c = board.I2C()
+
+display_bus = i2cdisplaybus.I2CDisplayBus(i2c, device_address=0x3C)
+
+display = adafruit_displayio_ssd1306.SSD1306(display_bus, width=128, height=64)
+
+bitmap = displayio.Bitmap(128, 64, 2)
+
+palette = displayio.Palette(2)
+palette[0] = 0x000000
+palette[1] = 0xFFFFFF
+
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+
+group = displayio.Group()
+group.append(tile_grid)
+
+display.root_group = group
+
+button = digitalio.DigitalInOut(board.D5)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.UP
+
+
+def draw_idle_face():
+    bitmap.fill(0)
+
+    for x in range(37, 43):
+        for y in range(25, 31):
+            bitmap[x, y] = 1
+
+    for x in range(85, 91):
+        for y in range(25, 31):
+            bitmap[x, y] = 1
+
+    for x in range(48, 80):
+        bitmap[x, 45] = 1
+
+
+def draw_focus_face():
+    bitmap.fill(0)
+
+    for x in range(37, 43):
+        bitmap[x, 27] = 1
+
+    for x in range(85, 91):
+        bitmap[x, 27] = 1
+
+    for x in range(48, 80):
+        bitmap[x, 45] = 1
+
+
+eyes_closed = False
+
+draw_idle_face()
+
+timer_running = False
+start_time = 0
+focus_length = 10
+
+while True:
+    if not button.value:
+        timer_running = not timer_running
+
+        if timer_running:
+            start_time = time.monotonic()
+            draw_focus_face()
+        else:
+            draw_idle_face()
+
+        while not button.value:
+            pass
+
+    if timer_running:
+        elapsed_time = time.monotonic() - start_time
+
+        if elapsed_time >= focus_length:
+            timer_running = False
+            draw_idle_face()
